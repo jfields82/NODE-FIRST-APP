af37b65e39f0801da1ae6dd2ea968991f9357647
justin <justinbradleyfields@gmail.com>
2023-02-26 01:10:17 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+const Server = http.createServer((req, res) => {
+    const url = req.url;
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title><head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+    return res.end();
+    }
+    res.setHeader('Content-type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title><head>');
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+    res.write('</html>');
+    res.end();
+}); 
+ 
+Server.listen(3000);
